fa7f656e30aad671b250f89156095590def3ad9b
usmansbk <usmansbk@gmail.com>
2023-01-18 16:36:59 +0100

    update schema

--- prisma/migrations/20230115235714_add_event_dates/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `startAt` to the `Event` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Event" ADD COLUMN     "endAt" TIMESTAMP(3),
-ADD COLUMN     "startAt" TIMESTAMP(3) NOT NULL;
--- prisma/migrations/20230118153647_init/migration.sql
+++ prisma/migrations/20230118153647_init/migration.sql
@@ -2,11 +2,46 @@
 CREATE TYPE "EventType" AS ENUM ('DEFAULT');
 
 -- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "emailVerified" BOOLEAN NOT NULL DEFAULT false,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT,
+    "language" TEXT,
+    "timezone" TEXT,
+    "pictureUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "File" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "bucket" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "size" INTEGER NOT NULL,
+    "mimetype" TEXT NOT NULL,
+    "userAvatarId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Event" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT,
     "code" TEXT,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "startTime" TEXT,
+    "endTime" TEXT,
+    "timezone" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "timetableId" TEXT,
@@ -40,6 +75,12 @@ CREATE TABLE "SavedTimetable" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "File_userAvatarId_key" ON "File"("userAvatarId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Event_code_key" ON "Event"("code");
 
 -- CreateIndex
@@ -47,6 +88,9 @@ CREATE UNIQUE INDEX "Timetable_code_key" ON "Timetable"("code");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "SavedTimetable_userId_timetableId_key" ON "SavedTimetable"("userId", "timetableId");
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_userAvatarId_fkey" FOREIGN KEY ("userAvatarId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Event" ADD CONSTRAINT "Event_timetableId_fkey" FOREIGN KEY ("timetableId") REFERENCES "Timetable"("id") ON DELETE SET NULL ON UPDATE CASCADE;
