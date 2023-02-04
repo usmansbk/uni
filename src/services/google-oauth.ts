1807cac7a2f7c03915fc8063199cce35ca4470fb
usmansbk <usmansbk@gmail.com>
2023-02-04 10:49:36 +0100

    bump

--- src/services/google-oauth.ts
+++ src/services/google-oauth.ts
@@ -1,7 +1,7 @@
-import { OAuth2Client } from "google-auth-library";
-import { INVALID_TOKEN_ERROR } from "~constants/errors";
+import { LoginTicket, OAuth2Client } from "google-auth-library";
 import { MISSING_NAME_AND_EMAIL } from "~constants/responseCodes";
 import type { UserPayload } from "~types";
+import { INVALID_TOKEN_ERROR } from "~constants/errors";
 import AuthenticationError from "~utils/errors/AuthenticationError";
 
 export default async function verifyGoogleCode(
@@ -13,25 +13,27 @@ export default async function verifyGoogleCode(
     redirectUri: "postmessage",
   });
 
+  let ticket: LoginTicket;
+
   try {
-    const ticket = await client.verifyIdToken({
+    ticket = await client.verifyIdToken({
       idToken,
     });
-
-    const payload = ticket.getPayload();
-
-    if (!(payload?.given_name && payload?.email)) {
-      throw new AuthenticationError(MISSING_NAME_AND_EMAIL);
-    }
-
-    return {
-      firstName: payload.given_name!,
-      lastName: payload.family_name!,
-      email: payload.email!,
-      emailVerified: !!payload.email_verified,
-      pictureUrl: payload.picture,
-    };
   } catch (e) {
     throw new AuthenticationError(INVALID_TOKEN_ERROR, e as Error);
   }
+
+  const payload = ticket.getPayload();
+
+  if (!(payload?.given_name && payload?.email)) {
+    throw new AuthenticationError(MISSING_NAME_AND_EMAIL);
+  }
+
+  return {
+    firstName: payload.given_name!,
+    lastName: payload.family_name!,
+    email: payload.email!,
+    emailVerified: !!payload.email_verified,
+    pictureUrl: payload.picture,
+  };
 }
